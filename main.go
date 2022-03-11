e35e3e99e6753804cb3e5c8eaf5fb2b8a8ff51c4
ducnamsavvycom <duc.nam@savvycomsoftware.com>
2022-03-11 10:34:56 +0700

    test

--- main.go
+++ main.go
@@ -56,7 +56,7 @@ func runService(db *gorm.DB, secretKey string, emailUsername string, emailPasswo
 			"message": "pong",
 		})
 	})
-
+	// CRUD
 	v1 := r.Group("/v1")
 
 	v1.POST("/register", ginuser.Register(appCtx))
